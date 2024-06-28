3375f51426d965a6ef583642212191d3b0327622
Frank Mwesigwa <frank.mwesigwa1@gmail.com>
2024-06-28 11:28:58 +0300

    new changes

--- routes/PatientRoutes.js
+++ routes/PatientRoutes.js
@@ -8,7 +8,7 @@ import {
 
 const router = express.Router();
 
-const serverUrl = 'http://104.248.21.177:8080/fhir';
+const serverUrl = 'http://165.227.170.84:9091/fhir';
 const resourceType = 'Patient';
 const uniqueId = uuidv4()
 
